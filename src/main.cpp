c55af9b5d80735cd44ebd6189ad8fbe1fd9016d4
joshuasymons-webb <000812836@mohawkcollege.ca>
2022-11-04 13:59:24 -0400

    initial commmit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+//Requirements
+
+//Displays the current temperature and updates it every 5 seconds
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
